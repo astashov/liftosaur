23240f792e8f9895343649f06e98644cbb684896
Anton Astashov <anton.astashov@gmail.com>
2020-04-18 21:50:08 -0500

    Add explicit registration of webpushr-sw.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,9 @@
 import { h, render } from "preact";
 import { AppView } from "./components/app";
 
+if ("serviceWorker" in navigator) {
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  navigator.serviceWorker.register("/webpushr-sw.js");
+}
+
 render(<AppView />, document.getElementById("app")!);
