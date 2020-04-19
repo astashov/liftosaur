774b3e9cd686e4a9b2403ebb6c7a3f66db7f98b1
Anton Astashov <anton.astashov@gmail.com>
2020-04-19 15:58:19 -0500

    Fix path to server

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,9 @@ module.exports = {
     new DefinePlugin({
       __COMMIT_HASH__: JSON.stringify(commitHash),
       __API_HOST__: JSON.stringify(
-        process.env.NODE_ENV === "production" ? "https://server.liftosaur.workers.dev" : "http://localhost:8787"
+        process.env.NODE_ENV === "production"
+          ? "https://server-production.liftosaur.workers.dev"
+          : "http://localhost:8787"
       )
     }),
     new CopyPlugin([
