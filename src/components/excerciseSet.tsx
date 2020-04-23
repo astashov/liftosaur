9d2a709c5db8a7d2251b1b9667cb52fb886ac0bc
Anton Astashov <anton.astashov@gmail.com>
2020-04-22 21:06:23 -0500

    Switch to onclick in <ExcerciseSet/>

--- src/components/excerciseSet.tsx
+++ src/components/excerciseSet.tsx
@@ -44,12 +44,12 @@ export function ExcerciseSetView(props: IProps): JSX.Element {
 function NotStartedExcerciseSet(props: INotStartedExcerciseSetProps): JSX.Element {
   return (
     <button
-      className="rounded-lg border border-gray-400 bg-gray-300 text-center w-12 h-12 leading-7 mr-3 my-2"
-      onTouchEnd={props.onClick}
+      className="w-12 h-12 my-2 mr-3 leading-7 text-center bg-gray-300 border border-gray-400 rounded-lg"
+      onClick={props.onClick}
       style={{ userSelect: "none", touchAction: "manipulation" }}
     >
       <div className="leading-none">{Reps.displayReps(props.number)}</div>
-      <div style={{ paddingTop: "2px" }} className="leading-none text-xs text-gray-600">
+      <div style={{ paddingTop: "2px" }} className="text-xs leading-none text-gray-600">
         {props.weight}
       </div>
     </button>
@@ -59,12 +59,12 @@ function NotStartedExcerciseSet(props: INotStartedExcerciseSetProps): JSX.Elemen
 function CompleteExcerciseSet(props: IStartedExcerciseSetProps): JSX.Element {
   return (
     <button
-      className="rounded-lg border border-green-400 bg-green-300 text-center w-12 h-12 leading-7 mr-3 my-2"
-      onTouchEnd={props.onClick}
+      className="w-12 h-12 my-2 mr-3 leading-7 text-center bg-green-300 border border-green-400 rounded-lg"
+      onClick={props.onClick}
       style={{ userSelect: "none", touchAction: "manipulation" }}
     >
       <div className="leading-none">{Reps.displayReps(props.number)}</div>
-      <div style={{ paddingTop: "2px" }} className="leading-none text-xs text-gray-600">
+      <div style={{ paddingTop: "2px" }} className="text-xs leading-none text-gray-600">
         {props.weight}
       </div>
     </button>
@@ -74,12 +74,12 @@ function CompleteExcerciseSet(props: IStartedExcerciseSetProps): JSX.Element {
 function IncompleteExcerciseSet(props: IStartedExcerciseSetProps): JSX.Element {
   return (
     <button
-      className="rounded-lg border border-red-400 bg-red-300 text-center w-12 h-12 leading-7 mr-3 my-2"
-      onTouchEnd={props.onClick}
+      className="w-12 h-12 my-2 mr-3 leading-7 text-center bg-red-300 border border-red-400 rounded-lg"
+      onClick={props.onClick}
       style={{ userSelect: "none", touchAction: "manipulation" }}
     >
       <div className="leading-none">{Reps.displayReps(props.number)}</div>
-      <div style={{ paddingTop: "2px" }} className="leading-none text-xs text-gray-600">
+      <div style={{ paddingTop: "2px" }} className="text-xs leading-none text-gray-600">
         {props.weight}
       </div>
     </button>
@@ -89,28 +89,24 @@ function IncompleteExcerciseSet(props: IStartedExcerciseSetProps): JSX.Element {
 function AmrapExcerciseSet(props: IAmrapExcerciseSetProps): JSX.Element {
   let className: string;
   if (props.number == null) {
-    className = "relative rounded-lg border border-gray-400 bg-gray-300 text-center w-12 h-12 leading-7 mr-3 my-2";
+    className = "relative w-12 h-12 my-2 mr-3 leading-7 text-center bg-gray-300 border border-gray-400 rounded-lg";
   } else if (props.number === 0) {
-    className = "relative rounded-lg border border-red-400 bg-red-300 text-center w-12 h-12 leading-7 mr-3 my-2";
+    className = "relative w-12 h-12 my-2 mr-3 leading-7 text-center bg-red-300 border border-red-400 rounded-lg";
   } else {
-    className = "relative rounded-lg border border-green-400 bg-green-300 text-center w-12 h-12 leading-7 mr-3 my-2";
+    className = "relative w-12 h-12 my-2 mr-3 leading-7 text-center bg-green-300 border border-green-400 rounded-lg";
   }
   return (
-    <button
-      className={className}
-      onTouchEnd={props.onClick}
-      style={{ userSelect: "none", touchAction: "manipulation" }}
-    >
+    <button className={className} onClick={props.onClick} style={{ userSelect: "none", touchAction: "manipulation" }}>
       {props.number != null && (
         <div
           style={{ top: "-0.5rem", right: "-0.5rem" }}
-          className="absolute leading-none text-right text-xs bg-gray-600 border-gray-800 rounded-full p-1 text-white"
+          className="absolute p-1 text-xs leading-none text-right text-white bg-gray-600 border-gray-800 rounded-full"
         >
           1+
         </div>
       )}
       <div className="leading-none">{props.number == null ? "1+" : props.number}</div>
-      <div style={{ paddingTop: "2px" }} className="leading-none text-xs text-gray-600">
+      <div style={{ paddingTop: "2px" }} className="text-xs leading-none text-gray-600">
         {props.weight}
       </div>
     </button>
