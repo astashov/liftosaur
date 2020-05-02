96932ba5ab1c452cc54835059c6bc6778f9ab274
Anton Astashov <anton.astashov@gmail.com>
2020-05-02 15:24:18 -0500

    Make clickable area of settings button bigger

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,10 +4,14 @@ import { IDispatch } from "../ducks/types";
 
 export function FooterView(props: { dispatch: IDispatch }): JSX.Element {
   return (
-    <div className="relative flex items-center p-3 pb-5 text-center text-white bg-blue-700">
-      <div className="flex-1 text-sm text-left text-blue-500">{__COMMIT_HASH__}</div>
+    <div className="relative flex items-center pb-2 text-center text-white bg-blue-700">
+      <div className="flex-1 px-3 text-sm text-left text-blue-500">{__COMMIT_HASH__}</div>
       <div className="flex-1 text-right">
-        <button aria-label="Settings" onClick={() => props.dispatch({ type: "PushScreen", screen: "settings" })}>
+        <button
+          className="p-4"
+          aria-label="Settings"
+          onClick={() => props.dispatch({ type: "PushScreen", screen: "settings" })}
+        >
           <IconCog />
         </button>
       </div>
