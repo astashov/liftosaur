f3093938b22f8507723e36c70eca585851bd4172
Anton Astashov <anton.astashov@gmail.com>
2020-05-02 15:19:15 -0500

    Add a bit padding to the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import { IDispatch } from "../ducks/types";
 
 export function FooterView(props: { dispatch: IDispatch }): JSX.Element {
   return (
-    <div className="relative flex items-center p-3 text-center text-white bg-blue-700">
+    <div className="relative flex items-center p-3 pb-5 text-center text-white bg-blue-700">
       <div className="flex-1 text-sm text-left text-blue-500">{__COMMIT_HASH__}</div>
       <div className="flex-1 text-right">
         <button aria-label="Settings" onClick={() => props.dispatch({ type: "PushScreen", screen: "settings" })}>
