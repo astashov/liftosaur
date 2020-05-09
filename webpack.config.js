7302b17bfdfc40bb0e956b3860117018ed2a5023
Anton Astashov <anton.astashov@gmail.com>
2020-05-09 15:10:26 -0500

    Add bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,28 @@
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
 const path = require("path");
 const { DefinePlugin } = require("webpack");
 const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const commitHash = require("child_process")
-  .execSync("git rev-parse --short HEAD")
-  .toString()
-  .trim();
+const commitHash = require("child_process").execSync("git rev-parse --short HEAD").toString().trim();
 
 // Export a function. Accept the base config as the only param.
 module.exports = {
   entry: {
     main: ["./src/index.tsx", "./src/index.css"],
-    "webpushr-sw": "./src/webpushr-sw.ts"
+    "webpushr-sw": "./src/webpushr-sw.ts",
   },
   output: {
     filename: "[name].js",
     publicPath: "/",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
   },
   devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
       {
         test: /\.tsx?$/,
@@ -32,15 +31,15 @@ module.exports = {
             loader: "ts-loader",
             options: {
               transpileOnly: true,
-              configFile: "tsconfig.json"
-            }
-          }
-        ]
-      }
-    ]
+              configFile: "tsconfig.json",
+            },
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".css"]
+    extensions: [".ts", ".tsx", ".js", ".css"],
   },
   plugins: [
     new MiniCssExtractPlugin(),
@@ -48,29 +47,30 @@ module.exports = {
       __COMMIT_HASH__: JSON.stringify(commitHash),
       __API_HOST__: JSON.stringify(
         process.env.NODE_ENV === "production" ? "https://api.liftosaur.com" : "http://localhost:8787"
-      )
+      ),
     }),
     new CopyPlugin([
       {
         from: `src/index.html`,
         to: `index.html`,
-        transform: content => {
+        transform: (content) => {
           return content.toString().replace(/\?version=xxxxxxxx/g, `?version=${commitHash}`);
-        }
+        },
       },
       {
         from: `src/notification.m4r`,
-        to: `notification.m4r`
+        to: `notification.m4r`,
       },
       {
         from: "icons",
-        to: "icons"
+        to: "icons",
       },
       {
         from: "manifest.webmanifest",
-        to: "manifest.webmanifest"
-      }
-    ])
+        to: "manifest.webmanifest",
+      },
+    ]),
+    new BundleAnalyzerPlugin({ analyzerMode: "static", openAnalyzer: false }),
   ],
   devServer: {
     contentBase: path.join(__dirname, "dist"),
@@ -79,6 +79,6 @@ module.exports = {
     inline: false,
     liveReload: false,
     host: "0.0.0.0",
-    disableHostCheck: true
-  }
+    disableHostCheck: true,
+  },
 };
