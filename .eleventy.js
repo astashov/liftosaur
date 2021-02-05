1c57f74482087ab14343053c4886ee7c82e43a7c
Anton Astashov <anton.astashov@gmail.com>
2021-02-04 20:44:03 -0600

    Add RSS for blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function (config) {
   config.addPassthroughCopy("blog/styles");
   config.addPassthroughCopy("blog/images");
+  config.addPlugin(pluginRss);
 
   config.dir = {
     input: "blog",
