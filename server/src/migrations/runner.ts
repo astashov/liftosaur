655d2197d285837a00bf4bad24852515f4c16139
Anton Astashov <anton.astashov@gmail.com>
2021-04-17 12:34:29 -0500

    Remove old cloudflare workers server code

--- server/src/migrations/runner.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectUtils } from "../../../src/utils/object";
-import { migrations } from "./migrations";
-
-export async function runMigrations(): Promise<void> {
-  const versions = ObjectUtils.keys(migrations);
-  for (const version of versions) {
-    await migrations[version]();
-  }
-}
--- server/src/render.tsx
+++
@@ -1,6 +0,0 @@
-import { JSX } from "preact";
-import render from "preact-render-to-string";
-
-export function renderPage(page: JSX.Element): string {
-  return "<!DOCTYPE html>" + render(page);
-}
