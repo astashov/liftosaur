278cfc265312333093538adc6dedfe5e0804de54
Anton Astashov <anton.astashov@gmail.com>
2022-12-30 14:19:23 -0600

    Add window types to lambda so it'd compile

---
+++ lambda/types/window.d.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/naming-convention
+interface Window {
+  _webpushrScriptReady: () => void;
+  webpushr: (name: "fetch_id", fn: (sid: number) => void) => void;
+  handleGapiLoad: () => void;
+  AppleID: {
+    auth: {
+      signIn: () => Promise<{
+        authorization: { code: string; id_token: string };
+      }>;
+      init: (args: { clientId: string; scope: string; redirectURI: string; usePopup: boolean }) => void;
+    };
+  };
+}
