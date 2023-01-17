afecaedb7118cdb630f2a7e1fbe7cd18b47c5d47
Anton Astashov <anton.astashov@gmail.com>
2023-01-16 22:30:35 -0600

    Fix export on iOS

--- src/utils/exporter.ts
+++ src/utils/exporter.ts
@@ -2,7 +2,10 @@ import { SendMessage } from "./sendMessage";
 
 export namespace Exporter {
   export function toFile(filename: string, contents: string): void {
-    if (!SendMessage.toAndroid({ type: "export", filename, contents })) {
+    if (
+      !SendMessage.toAndroid({ type: "export", filename, contents }) &&
+      !SendMessage.toIos({ type: "export", filename, contents })
+    ) {
       const blob = new Blob([contents], { type: "text/plain" });
       const objectUrl = URL.createObjectURL(blob);
       const a = document.createElement("a");
