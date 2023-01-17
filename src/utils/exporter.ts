1e611f7eafc36f91bd7a8f6109024f5ea5a82f71
Anton Astashov <anton.astashov@gmail.com>
2023-01-16 21:18:19 -0600

    Fix import/export on Android

--- src/utils/exporter.ts
+++ src/utils/exporter.ts
@@ -1,12 +1,16 @@
+import { SendMessage } from "./sendMessage";
+
 export namespace Exporter {
   export function toFile(filename: string, contents: string): void {
-    const blob = new Blob([contents], { type: "text/plain" });
-    const objectUrl = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = objectUrl;
-    a.setAttribute("download", filename);
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+    if (!SendMessage.toAndroid({ type: "export", filename, contents })) {
+      const blob = new Blob([contents], { type: "text/plain" });
+      const objectUrl = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = objectUrl;
+      a.setAttribute("download", filename);
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
   }
 }
