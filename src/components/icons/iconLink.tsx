9e95fed04e4ad3a725174e52f9829fce83c5e009
Anton Astashov <anton.astashov@gmail.com>
2023-03-03 20:50:14 -0800

    Add instructions how to import program on /program

--- src/components/icons/iconLink.tsx
+++ src/components/icons/iconLink.tsx
@@ -3,13 +3,21 @@ import { h, JSX } from "preact";
 interface IProps {
   size?: number;
   color?: string;
+  className?: string;
 }
 
 export function IconLink(props: IProps): JSX.Element {
   const size = props.size ?? 24;
   const color = props.color ?? "#171718";
   return (
-    <svg width={size} height={size} viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      className={props.className}
+      width={size}
+      height={size}
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path
         d="M31.1109 9.11091C33.2587 6.96303 36.7412 6.96303 38.889 9.11091C41.0369 11.2588 41.0369 14.7412 38.889 16.8891L30.639 25.1391C28.4912 27.287 25.0087 27.287 22.8609 25.1391C21.7869 24.0651 20.0457 24.0651 18.9718 25.1391C17.8978 26.213 17.8978 27.9542 18.9718 29.0282C23.2675 33.3239 30.2324 33.3239 34.5281 29.0282L42.7781 20.7782C47.0739 16.4824 47.0739 9.51759 42.7781 5.22183C38.4824 0.926058 31.5175 0.926058 27.2218 5.22183L23.0968 9.34682C22.0228 10.4208 22.0228 12.162 23.0968 13.2359C24.1707 14.3099 25.9119 14.3099 26.9859 13.2359L31.1109 9.11091Z"
         fill={color}
--- src/pages/program/programContent.tsx
+++ src/pages/program/programContent.tsx
@@ -34,6 +34,7 @@ import { IconCog2 } from "../../components/icons/iconCog2";
 import { ProgramContentModalSettings } from "./components/programContentModalSettings";
 import { EditExerciseUtil } from "./utils/editExerciseUtil";
 import { ProgramContentModalExerciseExamples } from "./components/programContentModalExerciseExamples";
+import { IconLink } from "../../components/icons/iconLink";
 
 export interface IProgramContentProps {
   client: Window["fetch"];
@@ -115,6 +116,46 @@ export function ProgramContent(props: IProgramContentProps): JSX.Element {
     : undefined;
   return (
     <section className="px-4 py-2">
+      <div className="flex items-center px-8 py-4 mb-4 bg-yellow-100 border border-orange-400 rounded-lg">
+        <div className="flex-1">
+          To use this program:
+          <ul className="pl-4 list-disc">
+            <li>Install Liftosaur app</li>
+            <li>
+              Copy the link to this program by clicking on <IconLink className="inline-block" size={16} /> below
+            </li>
+            <li>
+              Import the link in the app, on the <strong>Choose Program</strong> screen.
+            </li>
+          </ul>
+        </div>
+        <div className="ml-4">
+          <a
+            href="https://apps.apple.com/us/app/liftosaur/id1661880849?itsct=apps_box_badge&amp;itscg=30200"
+            target="_blank"
+            style="display: inline-block; overflow: hidden; border-radius: 13px"
+          >
+            <img
+              className="w-32"
+              src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1673481600"
+              alt="Download on the App Store"
+              style="border-radius: 13px"
+            />
+          </a>
+        </div>
+        <div style={{ marginTop: "-6px" }}>
+          <a
+            target="_blank"
+            href="https://play.google.com/store/apps/details?id=com.liftosaur.www.twa&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
+          >
+            <img
+              className="w-40"
+              alt="Get it on Google Play"
+              src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+            />
+          </a>
+        </div>
+      </div>
       <div className="flex flex-col sm:flex-row">
         <div className="flex-1 min-w-0">
           <div className="flex items-center">
