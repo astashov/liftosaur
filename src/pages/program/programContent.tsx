ee5b53edcdaed554dbc21046821f3fc5af92f6e0
Anton Astashov <anton.astashov@gmail.com>
2023-03-03 21:19:32 -0800

    Fix layout of /program on mobile

--- src/pages/program/programContent.tsx
+++ src/pages/program/programContent.tsx
@@ -116,7 +116,7 @@ export function ProgramContent(props: IProgramContentProps): JSX.Element {
     : undefined;
   return (
     <section className="px-4 py-2">
-      <div className="flex items-center px-8 py-4 mb-4 bg-yellow-100 border border-orange-400 rounded-lg">
+      <div className="flex flex-col items-center px-8 py-4 mb-4 bg-yellow-100 border border-orange-400 rounded-lg sm:flex-row">
         <div className="flex-1">
           To use this program:
           <ul className="pl-4 list-disc">
@@ -129,31 +129,33 @@ export function ProgramContent(props: IProgramContentProps): JSX.Element {
             </li>
           </ul>
         </div>
-        <div className="ml-4">
-          <a
-            href="https://apps.apple.com/us/app/liftosaur/id1661880849?itsct=apps_box_badge&amp;itscg=30200"
-            target="_blank"
-            style="display: inline-block; overflow: hidden; border-radius: 13px"
-          >
-            <img
-              className="w-32"
-              src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1673481600"
-              alt="Download on the App Store"
-              style="border-radius: 13px"
-            />
-          </a>
-        </div>
-        <div style={{ marginTop: "-6px" }}>
-          <a
-            target="_blank"
-            href="https://play.google.com/store/apps/details?id=com.liftosaur.www.twa&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
-          >
-            <img
-              className="w-40"
-              alt="Get it on Google Play"
-              src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
-            />
-          </a>
+        <div className="flex items-center mt-2 ml-4">
+          <div>
+            <a
+              href="https://apps.apple.com/us/app/liftosaur/id1661880849?itsct=apps_box_badge&amp;itscg=30200"
+              target="_blank"
+              style="display: inline-block; overflow: hidden; border-radius: 13px"
+            >
+              <img
+                className="w-32"
+                src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1673481600"
+                alt="Download on the App Store"
+                style="border-radius: 13px"
+              />
+            </a>
+          </div>
+          <div style={{ marginTop: "-6px" }}>
+            <a
+              target="_blank"
+              href="https://play.google.com/store/apps/details?id=com.liftosaur.www.twa&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
+            >
+              <img
+                className="w-40"
+                alt="Get it on Google Play"
+                src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+              />
+            </a>
+          </div>
         </div>
       </div>
       <div className="flex flex-col sm:flex-row">
