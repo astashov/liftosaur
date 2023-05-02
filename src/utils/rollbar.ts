f875c8c4a4db42254f917dc93a315aebd5bd3bba
Anton Astashov <anton.astashov@gmail.com>
2023-05-02 12:56:35 -0500

    Add more ignored errors

--- src/utils/rollbar.ts
+++ src/utils/rollbar.ts
@@ -19,6 +19,7 @@ export namespace RollbarUtils {
           "The operation was aborted",
           "FetchEvent.respondWith received an error",
           "play() failed because the user",
+          "The request is not allowed by the user agent or the platform",
         ];
         const firstArg = args[0];
         if (firstArg && typeof firstArg === "string" && ignores.some((i) => firstArg.indexOf(i) === 0)) {
