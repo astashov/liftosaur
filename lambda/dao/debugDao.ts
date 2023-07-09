62b8f22e2df9b365d76bb69fbf94e102c441edb3
Anton Astashov <anton.astashov@gmail.com>
2023-07-09 09:08:40 -0500

    Add a way to fetch debug info

--- lambda/dao/debugDao.ts
+++ lambda/dao/debugDao.ts
@@ -18,6 +18,20 @@ export interface IDebugDao {
 export class DebugDao {
   constructor(private readonly di: IDI) {}
 
+  public async get(id: string): Promise<IDebugDao | undefined> {
+    const env = Utils.getEnv();
+    const result = await this.di.s3.getObject({
+      bucket: bucketNames[env].debug,
+      key: `debuginfo/${id}`,
+    });
+    if (result) {
+      const state = result.toString();
+      return { id, state };
+    } else {
+      return undefined;
+    }
+  }
+
   public async store(id: string, state: string): Promise<void> {
     const env = Utils.getEnv();
     await this.di.s3.putObject({
