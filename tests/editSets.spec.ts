13bd65ec5e99185b7dcbfc22139984faeec89819
Anton Astashov <anton.astashov@gmail.com>
2024-06-17 22:09:50 -0500

    Migrate another 3 tests to playwright

---
+++ tests/editSets.spec.ts
@@ -0,0 +1,83 @@
+import { test, expect } from "@playwright/test";
+
+test("edits sets properly", async ({ page }) => {
+  page.on("dialog", (dialog) => dialog.accept());
+  await page.goto("https://local.liftosaur.com:8080/app/?skipintro=1");
+  await page.locator("button:has-text('Basic Beginner Routine')").click();
+  await page.getByTestId("clone-program").click();
+  await page.getByTestId("start-workout").click();
+
+  await page.getByTestId("exercise-edit-mode").nth(1).click();
+  await page.getByTestId("modal-edit-mode-this-workout").click();
+  await page.getByTestId("add-warmup-set").click({ force: true });
+  await page.getByTestId("modal-edit-set-reps-input").fill("10");
+  await page.getByTestId("modal-edit-set-weight-input").fill("100");
+  await page.getByTestId("modal-edit-set-submit").click();
+
+  await page.getByTestId("add-workout-set").click({ force: true });
+  await page.getByTestId("modal-edit-set-reps-input").fill("20");
+  await page.getByTestId("modal-edit-set-weight-input").fill("200");
+  await page.getByTestId("modal-edit-set-submit").click();
+
+  await page.getByTestId("set-edit-mode-remove").nth(2).click({ force: true });
+  await page
+    .locator("[data-cy^=exercise-]:has-text('Bench Press') [data-cy=set-nonstarted]")
+    .nth(1)
+    .click({ force: true });
+  await page.getByTestId("modal-edit-set-reps-input").fill("8");
+  await page.getByTestId("modal-edit-set-weight-input").fill("80");
+  await page.getByTestId("modal-edit-set-submit").click();
+
+  await page.getByTestId("done-edit-exercise").click();
+
+  // Checking the result
+
+  const setsSelector = "[data-cy^=exercise-]:has-text('Bench Press') [data-cy^=set-]";
+  await expect(page.locator(setsSelector).nth(0).locator("[data-cy=reps-value]")).toHaveText("10");
+  await expect(page.locator(setsSelector).nth(0).locator("[data-cy=weight-value]")).toHaveText("100");
+  await expect(page.locator(setsSelector).nth(0)).toHaveAttribute("data-cy", "set-nonstarted");
+
+  await expect(page.locator(setsSelector).nth(1).locator("[data-cy=reps-value]")).toHaveText("8");
+  await expect(page.locator(setsSelector).nth(1).locator("[data-cy=weight-value]")).toHaveText("80");
+  await expect(page.locator(setsSelector).nth(1)).toHaveAttribute("data-cy", "set-nonstarted");
+
+  await expect(page.locator(setsSelector).nth(2).locator("[data-cy=reps-value]")).toHaveText("5+");
+  await expect(page.locator(setsSelector).nth(2).locator("[data-cy=weight-value]")).toHaveText("45");
+  await expect(page.locator(setsSelector).nth(2)).toHaveAttribute("data-cy", "set-amrap-nonstarted");
+
+  await expect(page.locator(setsSelector).nth(3).locator("[data-cy=reps-value]")).toHaveText("20+");
+  await expect(page.locator(setsSelector).nth(3).locator("[data-cy=weight-value]")).toHaveText("200");
+  await expect(page.locator(setsSelector).nth(3)).toHaveAttribute("data-cy", "set-amrap-nonstarted");
+
+  // Adding and deleting exercises
+
+  await page.getByTestId("entry-bent-over-row").locator("[data-cy=exercise-edit-mode]").click();
+  await page.getByTestId("modal-edit-mode-this-workout").click();
+  await page.getByTestId("delete-edit-exercise").click();
+  await expect(page.getByTestId("entry-bent-over-row")).not.toBeVisible();
+
+  await page.getByTestId("add-exercise-button").click();
+  await page.getByTestId("menu-item-arnold-press-kettlebell").click();
+
+  await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+  await page.getByTestId("add-workout-set").and(page.locator(":visible")).click({ force: true });
+  await page.getByTestId("modal-edit-set-reps-input").fill("8");
+  await page.getByTestId("modal-edit-set-weight-input").fill("250");
+  await page.getByTestId("modal-edit-set-submit").click();
+
+  const arnoldPressSelector = "[data-cy^=exercise-]:has-text('Arnold Press') [data-cy^=set-]";
+  await expect(page.locator(arnoldPressSelector).nth(0).locator("[data-cy=reps-value]")).toHaveText("8");
+  await expect(page.locator(arnoldPressSelector).nth(0).locator("[data-cy=weight-value]")).toHaveText("250");
+  await expect(page.locator(arnoldPressSelector).nth(0)).toHaveAttribute("data-cy", "set-nonstarted");
+
+  await page.getByTestId("entry-squat").locator("[data-cy=exercise-edit-mode]").click();
+  await page.getByTestId("menu-item-value-1-rep-max").fill("200");
+  await page.getByTestId("modal-edit-mode-save-statvars").click();
+
+  await expect(page.getByTestId("entry-bent-over-row")).not.toBeVisible();
+  await expect(page.getByTestId("entry-arnold-press")).toBeVisible();
+  await expect(page.locator(arnoldPressSelector).nth(0).locator("[data-cy=reps-value]")).toHaveText("8");
+
+  await page.locator("text=Finish the workout").click();
+  await page.locator("text=Continue").click();
+});
---
+++ tests/userPromptedStateVars.spec.ts
@@ -0,0 +1,79 @@
+import { test, expect } from "@playwright/test";
+import { PlaywrightUtils } from "./playwrightUtils";
+
+test("User Prompted State Vars", async ({ page }) => {
+  await page.goto("https://local.liftosaur.com:8080/app/?skipintro=1");
+  // Creating the program
+
+  await page.getByTestId("create-program").click();
+
+  await page.getByTestId("modal-create-program-input").clear();
+  await page.getByTestId("modal-create-program-input").type("A Program");
+  await page.getByTestId("modal-create-program-submit").click();
+
+  await page.getByTestId("add-exercise").click();
+  await page.getByTestId("tab-advanced").click();
+
+  await page.getByTestId("add-state-variable").click();
+  await page.getByTestId("modal-add-state-variable-input-name").clear();
+  await page.getByTestId("modal-add-state-variable-input-name").type("rpe");
+  await page.getByTestId("menu-item-name-user-prompted").click();
+  await page.getByTestId("modal-add-state-variable-submit").click();
+
+  await page.getByTestId("add-new-set").click();
+
+  await PlaywrightUtils.clearCodeMirror(page, "multiline-editor-finish-day");
+  await PlaywrightUtils.typeCodeMirror(
+    page,
+    "multiline-editor-finish-day",
+    "if (state.rpe > 7) {\n  state.weight = state.weight - 5lb\n} else if (state.rpe < 3) {\n  state.weight = state.weight + 5lb\n}"
+  );
+
+  await page.getByTestId("save-program").click();
+  await page.getByTestId("edit-day").click();
+  await page.getByTestId("menu-item-squat").click();
+
+  await page.getByTestId("footer-workout").click();
+  await page.getByTestId("start-workout").click();
+
+  PlaywrightUtils.clickAll(page.locator("[data-cy^=exercise-]:has-text('Squat') >> [data-cy^=set-]"));
+
+  await page.getByTestId("modal-state-vars-user-prompt-input-rpe").clear();
+  await page.getByTestId("modal-state-vars-user-prompt-input-rpe").type("8");
+
+  await page.getByTestId("modal-amrap-submit").click();
+
+  await expect(page.getByTestId("state-changes").first()).toContainText("weight: 135 lb -> 130 lb");
+  await expect(page.getByTestId("state-changes").first()).toContainText("rpe: 0 -> 8");
+
+  await page.locator("[data-cy^=exercise-]:has-text('Squat') >> [data-cy^=set-]").nth(4).click();
+  await page.locator("[data-cy^=exercise-]:has-text('Squat') >> [data-cy^=set-]").nth(4).click();
+  await page.locator("[data-cy^=exercise-]:has-text('Squat') >> [data-cy^=set-]").nth(4).click();
+  await page.locator("[data-cy^=exercise-]:has-text('Squat') >> [data-cy^=set-]").nth(4).click();
+  await page.locator("[data-cy^=exercise-]:has-text('Squat') >> [data-cy^=set-]").nth(4).click();
+  await page.locator("[data-cy^=exercise-]:has-text('Squat') >> [data-cy^=set-]").nth(4).click();
+  await page.locator("[data-cy^=exercise-]:has-text('Squat') >> [data-cy^=set-]").nth(4).click();
+
+  await page.getByTestId("modal-state-vars-user-prompt-input-rpe").clear();
+  await page.getByTestId("modal-state-vars-user-prompt-input-rpe").type("2");
+
+  await page.getByTestId("modal-amrap-submit").click();
+  await expect(page.getByTestId("state-changes").first()).toContainText("weight: 135 lb -> 140 lb");
+  await expect(page.getByTestId("state-changes").first()).toContainText("rpe: 0 -> 2");
+
+  await page.getByRole("button", { name: "Finish the workout" }).click();
+  await page.getByRole("button", { name: "Continue" }).click();
+
+  await page.getByTestId("start-workout").click();
+
+  await expect(
+    page.locator("[data-cy^=exercise-]:has-text('Squat') >> [data-cy^=set-]").nth(1).getByTestId("weight-value")
+  ).toContainText("70");
+
+  PlaywrightUtils.clickAll(page.locator("[data-cy^=exercise-]:has-text('Squat') >> [data-cy^=set-]"));
+  await page.getByTestId("modal-state-vars-user-prompt-input-rpe").clear();
+  await page.getByTestId("modal-state-vars-user-prompt-input-rpe").type("5");
+  await page.getByTestId("modal-amrap-submit").click();
+  await expect(page.getByTestId("state-changes").first()).not.toContainText("weight");
+  await expect(page.getByTestId("state-changes").first()).toContainText("rpe: 2 -> 5");
+});
