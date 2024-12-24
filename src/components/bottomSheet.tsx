f0d8480ee673ce134758841a451c7a6ba6059254
Anton Astashov <anton.astashov@gmail.com>
2024-12-23 18:27:43 -0700

    Fix bottomsheet

--- src/components/bottomSheet.tsx
+++ src/components/bottomSheet.tsx
@@ -8,7 +8,7 @@ interface IProps {
 }
 
 export function BottomSheet(props: IProps): JSX.Element {
-  const [bottomShift, setBottomShift] = useState(-99999);
+  const [bottomShift, setBottomShift] = useState(window.innerHeight);
   const bottomSheetRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -18,11 +18,11 @@ export function BottomSheet(props: IProps): JSX.Element {
   }, []);
 
   useEffect(() => {
-    setBottomShift(props.isHidden ? bottomShift : 0);
+    setBottomShift(props.isHidden ? bottomSheetRef.current?.clientHeight ?? 0 : 0);
   }, [props.isHidden]);
 
   return (
-    <div className={`fixed inset-0 z-30 ${props.isHidden ? "invisible " : ""}`}>
+    <div className={`fixed inset-0 z-30 ${props.isHidden ? "invisible " : "visible"}`}>
       <div
         data-name="overlay"
         className={`absolute inset-0 bg-grayv2-700 ${props.isHidden ? "opacity-0" : "opacity-50"}`}
@@ -32,6 +32,7 @@ export function BottomSheet(props: IProps): JSX.Element {
         ref={bottomSheetRef}
         className={`absolute left-0 bottom-0 flex w-full bg-white`}
         style={{
+          transition: "transform 0.1s ease-out, visibility 0.1s",
           transform: `translateY(${bottomShift}px)`,
           borderRadius: "16px 16px 0 0",
           boxShadow: "0 -5px 15px rgb(0 0 0 / 30%)",
