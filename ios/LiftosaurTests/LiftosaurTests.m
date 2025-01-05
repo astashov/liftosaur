4447d58354bf568c8d509ac2ec281c6ee00777a0
Anton Astashov <anton.astashov@gmail.com>
2025-01-04 19:39:15 -0600

    Rename ios app to Liftosaur

--- ios/LiftosaurTests/LiftosaurTests.m
+++ ios/LiftosaurTests/LiftosaurTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNappTests : XCTestCase
+@interface LiftosaurTests : XCTestCase
 
 @end
 
-@implementation RNappTests
+@implementation LiftosaurTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
