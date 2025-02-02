276464236e8ae485621a6b2419fa7885f0d8a9a6
Anton Astashov <anton.astashov@gmail.com>
2025-02-02 15:00:57 -0600

    Add padding for null state

--- src/components/historyRecordsNullState.tsx
+++ src/components/historyRecordsNullState.tsx
@@ -4,11 +4,13 @@ import { IconArrowDown3 } from "./icons/iconArrowDown3";
 export function HistoryRecordsNullState(): JSX.Element {
   return (
     <div className="fixed bottom-0 left-0 w-full py-8 pb-20 text-center border border-purple-300 bg-purplev3-50 rounded-2xl">
-      <img src="/images/dyno-smile.png" className="inline" style={{ width: 51, height: 51 }} />
-      <h3 className="py-2 font-semibold">Welcome to Liftosaur!</h3>
-      <div className="text-sm">Tap here to start a workout</div>
-      <div className="py-2">
-        <IconArrowDown3 className="inline-block" />
+      <div className="safe-area-inset-bottom">
+        <img src="/images/dyno-smile.png" className="inline" style={{ width: 51, height: 51 }} />
+        <h3 className="py-2 font-semibold">Welcome to Liftosaur!</h3>
+        <div className="text-sm">Tap here to start a workout</div>
+        <div className="py-2">
+          <IconArrowDown3 className="inline-block" />
+        </div>
       </div>
     </div>
   );
