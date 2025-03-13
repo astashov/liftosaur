c331f95b1f3135c9ac9d73d87219336e8a1c3f88
Anton Astashov <anton.astashov@gmail.com>
2025-03-12 21:06:55 -0500

    Ignore 'when attempting to fetch resource' error

--- src/utils/rollbar.ts
+++ src/utils/rollbar.ts
@@ -31,6 +31,7 @@ export const exceptionIgnores = [
   "Load failed",
   "Function timed out",
   "is out of range for changeset",
+  "when attempting to fetch resource",
   "Selection points outside of document",
   "Invalid position",
   "outdated_client_storage",
