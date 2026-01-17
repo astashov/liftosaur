46b482aee8fcd9e2ef1921272eb9ae327ea3d59f
Anton Astashov <anton.astashov@gmail.com>
2026-01-17 13:33:23 -0600

    Fix the auth syncing with watch

--- src/ducks/thunks.ts
+++ src/ducks/thunks.ts
@@ -157,7 +157,7 @@ export namespace Thunk {
         await env.service.signout();
         dispatch({ type: "Logout" });
         updateState(dispatch, [lb<IState>().p("lastSyncedStorage").record(undefined)], "Clear last sync on logout");
-        SendMessage.toIos({ type: "authChanged" });
+        SendMessage.toIos({ type: "accountLogout" });
       }
       if (cb) {
         cb();
@@ -1370,7 +1370,7 @@ async function handleLogin(
         dispatch({ type: "ReplaceState", state: newState });
       }
       dispatch(Thunk.fetchInitial());
-      SendMessage.toIos({ type: "authChanged" });
+      SendMessage.toIos({ type: "authChanged", userId: result.user_id });
     } else if (result.key) {
       updateState(
         dispatch,
