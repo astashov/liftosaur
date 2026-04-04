8141e3bb393f4488b8729eeba9c9a2b170e7588b
Anton Astashov <anton.astashov@gmail.com>
2026-04-04 16:56:20 -0500

    RN Modals: Share modals

---
+++ src/navigation/modals/NavModalSocialShare.tsx
@@ -0,0 +1,27 @@
+import { JSX } from "react";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import { useAppState } from "../StateContext";
+import { SheetScreenContainer } from "../SheetScreenContainer";
+import { WorkoutSocialShareSheet } from "../../components/workoutSocialShareSheet";
+import type { IRootStackParamList } from "../types";
+
+export function NavModalSocialShare(): JSX.Element {
+  const { state } = useAppState();
+  const navigation = useNavigation();
+  const route = useRoute<{ key: string; name: "socialShareModal"; params: IRootStackParamList["socialShareModal"] }>();
+  const { type } = route.params;
+
+  const record = state.storage.history[0];
+
+  return (
+    <SheetScreenContainer onClose={() => navigation.goBack()} shouldShowClose={true}>
+      <WorkoutSocialShareSheet
+        history={state.storage.history}
+        record={record}
+        settings={state.storage.settings}
+        type={type}
+        isHidden={false}
+      />
+    </SheetScreenContainer>
+  );
+}
