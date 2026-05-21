5797f7b91ef9391259b12969eccc880bc90b4f6d
Anton Astashov <anton.astashov@gmail.com>
2026-05-21 10:39:10 -0500

    Fix scrolling issues on the webview editors

--- src/components/editProgram/editProgramV2Full.tsx
+++ src/components/editProgram/editProgramV2Full.tsx
@@ -1,4 +1,4 @@
-import { JSX, useEffect, useMemo, useRef } from "react";
+import { JSX, useEffect, useMemo, useRef, useState } from "react";
 import { View } from "react-native";
 import { IPlannerProgram, ISettings } from "../../types";
 import { ILensDispatch } from "../../utils/useLensReducer";
@@ -21,13 +21,22 @@ export interface IEditProgramV2FullProps {
 }
 
 export function EditProgramV2Full(props: IEditProgramV2FullProps): JSX.Element {
-  const fulltext = PlannerProgram_generateFullText(props.plannerProgram.weeks);
+  const [fulltext, setFulltext] = useState(() => PlannerProgram_generateFullText(props.plannerProgram.weeks));
+  const expectedRegenRef = useRef(fulltext);
   const lbProgram = lb<IPlannerState>().p("current").p("program").pi("planner");
   const lbUi = lb<IPlannerState>().p("ui");
   const { evaluatedWeeks, exerciseFullNames } = useMemo(() => {
     return PlannerProgram_evaluateFull(fulltext, props.settings);
   }, [fulltext, props.settings]);
   const settingsRef = useRef(props.settings);
+
+  useEffect(() => {
+    const regen = PlannerProgram_generateFullText(props.plannerProgram.weeks);
+    if (regen === expectedRegenRef.current) return;
+    expectedRegenRef.current = regen;
+    setFulltext(regen);
+  }, [props.plannerProgram.weeks]);
+
   useEffect(() => {
     settingsRef.current = props.settings;
     const { evaluatedWeeks: evaluatedWeeks2 } = PlannerProgram_evaluateFull(fulltext, settingsRef.current);
@@ -43,45 +52,45 @@ export function EditProgramV2Full(props: IEditProgramV2FullProps): JSX.Element {
 
   return (
     <View className="relative">
-      <View className="flex-col px-4 pt-4">
-        <View className="flex-1">
-          <PlannerEditorView
-            name="Program"
-            autoHeight={true}
-            minHeight={200}
-            customExercises={props.settings.exercises}
-            exerciseFullNames={exerciseFullNames}
-            error={
-              props.ui.fullTextError
-                ? props.ui.fullTextError
-                : evaluatedWeeks.success
-                  ? undefined
-                  : evaluatedWeeks.error
-            }
-            value={fulltext}
-            onCustomErrorCta={(err) => (
-              <EditProgramCustomErrorCta
-                dayData={{ week: 1, dayInWeek: 1 }}
-                dispatch={props.plannerDispatch}
-                err={err}
-              />
-            )}
-            onChange={(text) => {
-              const weeks = PlannerProgram_evaluateText(text);
-              const { evaluatedWeeks: evaluatedWeeks2 } = PlannerProgram_evaluateFull(text, settingsRef.current);
-              props.plannerDispatch(
-                [
-                  lbUi.p("fullTextError").record(evaluatedWeeks2.success ? undefined : evaluatedWeeks2.error),
-                  lbProgram.p("weeks").record(weeks),
-                ],
-                "Update full program text"
-              );
-            }}
-            lineNumbers={true}
-            onBlur={(_e, _text) => {}}
-            onLineChange={(_line) => {}}
-          />
-        </View>
+      <View className="px-4 pt-4">
+        <PlannerEditorView
+          name="Program"
+          autoHeight={true}
+          minHeight={200}
+          customExercises={props.settings.exercises}
+          exerciseFullNames={exerciseFullNames}
+          error={
+            props.ui.fullTextError
+              ? props.ui.fullTextError
+              : evaluatedWeeks.success
+                ? undefined
+                : evaluatedWeeks.error
+          }
+          value={fulltext}
+          onCustomErrorCta={(err) => (
+            <EditProgramCustomErrorCta
+              dayData={{ week: 1, dayInWeek: 1 }}
+              dispatch={props.plannerDispatch}
+              err={err}
+            />
+          )}
+          onChange={(text) => {
+            setFulltext(text);
+            const weeks = PlannerProgram_evaluateText(text);
+            expectedRegenRef.current = PlannerProgram_generateFullText(weeks);
+            const { evaluatedWeeks: evaluatedWeeks2 } = PlannerProgram_evaluateFull(text, settingsRef.current);
+            props.plannerDispatch(
+              [
+                lbUi.p("fullTextError").record(evaluatedWeeks2.success ? undefined : evaluatedWeeks2.error),
+                lbProgram.p("weeks").record(weeks),
+              ],
+              "Update full program text"
+            );
+          }}
+          lineNumbers={true}
+          onBlur={(_e, _text) => {}}
+          onLineChange={(_line) => {}}
+        />
       </View>
     </View>
   );
